fa70b3fef98b10b79625afef6b509d23b173e9ee
NazarSadovij <145208436+NazarSadovij@users.noreply.github.com>
2024-05-18 14:17:59 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -6,10 +6,38 @@ info = display.Info()
 screen_width, screen_height = info.current_w, info.current_h 
 
 window = display.set_mode((screen_width, screen_height))
-background = transform.scale(image.load("background.jpg"), (screen_width, screen_height))
+background = transform.scale(image.load("background_menu.jpg"), (screen_width, screen_height))
 background_pause = transform.scale(image.load("pause.jpg"), (screen_width, screen_height))
 game = True
 pause = False
+
+
+class Hero (sprite.Sprite):
+    def __init__ (self, x, y, width, height, speed, img_name = "Players_1.jpg"):
+        self.image = image.load(img_name)
+        self.image = transform.scale(self.image, (width, height))
+        self.speed = speed
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        super().__init__()
+
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+class Player_1(Hero):
+    def move (self):
+        keys = key.get_pressed()
+        if keys [K_a] and self.rect.x > 5:
+            self.rect.x -= self.speed
+
+        if keys [K_d] and self.rect.x < 700:
+            self.rect.x += self.speed
+
+
+
+player_1 = Player_1 (400, 400, 100, 100, 5)
+
 
 clock = time.Clock()
 
@@ -20,6 +48,10 @@ def stop_game():
 btn1 =  Button(window, screen_width, screen_height,stop_game)
 
 while game:
+
+    player_1.reset()
+    player_1.move()
+
 
     if pause:
         btn1.click(e)
@@ -43,6 +75,7 @@ while game:
         continue
 
     
+
     window.blit(background, (0,0))
     display.update()
     clock.tick(60)
